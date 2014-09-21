e69cbdd26ca21ec77f024551e112b80d63305044
Andrew Barba <abarba.77@gmail.com>
2014-09-21 13:13:49 -0400

    setup dependencies

--- project_2/src/crawl.js
+++ project_2/src/crawl.js
@@ -1,2 +1,18 @@
-console.log('Web Crawler');
-console.log(process.argv);
+/**
+ * Project 2 Solution written in Node.js
+ */
+
+/*==========================================*
+/* Dependencies
+/*==========================================*/
+
+var net   = require('net')
+  , snet  = require('tls')
+  , util  = require('util')
+  , async = require('async')
+  , _     = require('underscore');
+
+/*==========================================*
+/* Constants
+/*==========================================*/
+
