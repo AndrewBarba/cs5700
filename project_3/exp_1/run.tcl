61905eae9b12648fca74f1822f8a101a0111d561
Andrew Barba <abarba.77@gmail.com>
2014-10-19 18:47:16 -0400

    ex

--- project_3/exp_1/run.tcl
+++ project_3/exp_1/run.tcl
@@ -82,7 +82,7 @@ $ns connect $udp $null
 ################################################################################
 
 # Add TCP agent to node 1
-set tcp [new Agent/TCP]
+set tcp [new Agent/TCP-Reno]
 $ns attach-agent $n1 $tcp
 
 # Add TCP sink to node 4
