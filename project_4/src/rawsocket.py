38216f4067b896621a200088b6ac54855258c0ca
Andrew Barba <abarba.77@gmail.com>
2014-11-12 17:50:43 -0500

    x

--- project_4/src/rawsocket.py
+++ project_4/src/rawsocket.py
@@ -138,7 +138,7 @@ class RawSocket():
 		return self.socket.close()
 
 	def __init__(self):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 		self.socket.bind(("0.0.0.0", 0))
 
 def rawsocket():
