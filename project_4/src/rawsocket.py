a706f4675349ce54f3e422921239481b6ac42e93
Andrew Barba <abarba.77@gmail.com>
2014-11-12 22:56:03 -0500

    cleanup

--- project_4/src/rawsocket.py
+++ project_4/src/rawsocket.py
@@ -48,13 +48,11 @@ class OutPacket():
         s = 0
         n = len(data) % 2
         for i in range(0, len(data)-n, 2):
-                s+= ord(data[i]) + (ord(data[i+1]) << 8)
+            s+= ord(data[i]) + (ord(data[i+1]) << 8)
         if n:
-                s+= ord(data[i+1])
+            s+= ord(data[i+1])
         while (s >> 16):
-                print("s >> 16: ", s >> 16)
-                s = (s & 0xFFFF) + (s >> 16)
-        print("sum:", s)
+            s = (s & 0xFFFF) + (s >> 16)
         s = ~s & 0xffff
         return s
 
